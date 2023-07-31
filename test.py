c041894913a3ddad0d7c6d7368f413c62d682b4f
ismailiyad <ismailiyad@gmail,com>
2023-07-31 14:58:51 +0300

    third commit

---
+++ test.py
@@ -0,0 +1,14 @@
+name = "ismail"
+age = 28
+length = 178
+maried = True
+Age = "25"
+
+print(type(name))
+print(type(length))
+print(type(age))
+print(type(maried))
+print(type(Age))
+
+Age = int(Age)
+print(type(Age))
---
+++ test_pkg/__init__.py
@@ -0,0 +1,8 @@
+name = input("Enter Your Name :")
+print(name)
+
+age = int(input("Enter Your age :"))
+print(age)
+
+date = (input("Enter Your Date :"))
+print(date)
