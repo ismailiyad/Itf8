e8a014e3c951dca261e6e7b8450d5ee2446a74c2
ismailiyad <ismailiyad@gmail,com>
2023-07-31 12:50:58 +0300

    second commit

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print("ismail")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print ("ismail")
+
+
