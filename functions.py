fab9edf2ce4a5e6674f94c8125437a9dce3c3a86
ismailiyad <ismailiyad@gmail,com>
2023-08-04 16:11:09 +0300

    third commit

---
+++ functions.py
@@ -0,0 +1,6 @@
+
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+def = sum(num1, num2):
+        print(sum)
+sum
---
+++ task2.py
@@ -0,0 +1,6 @@
+
+name = input("Enter Full name")
+mobile_number = input("Enter mobile number")
+year_of_birth = input("Enter Your Year of birth ")
+age = 2023 - int(year_of_birth)
+print(age)
