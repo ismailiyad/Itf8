294c9d799c16033086d82ee123f606f9bf1f2981
ismailiyad <ismailiyad@gmail,com>
2023-08-04 17:19:20 +0300

    third commit

--- functions.py
+++ functions.py
@@ -1,6 +1,23 @@
 
 num1 = int(input("Enter number 1"))
 num2 = int(input("Enter number 2"))
-def = sum(num1, num2):
-        print(sum)
-sum
+
+def sum_fun(num1, num2):
+        return num1 + num2
+
+def sub_fun(num1, num2):
+        return num1 - num2
+
+def multipl_fun(num1, num2):
+        return num1 * num2
+
+def div_fun(num1, num2):
+        return num1 / num2
+
+print(sum_fun(num1, num2))
+print(sub_fun(num1, num2))
+print(multipl_fun(num1, num2))
+print(div_fun(num1, num2))
+
+
+
