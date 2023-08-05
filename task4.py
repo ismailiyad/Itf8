d4b3b648fb3a8e1a35d8f4073d72eb95206b81f4
ismailiyad <ismailiyad@gmail,com>
2023-08-05 11:15:51 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ task4.py
@@ -0,0 +1,10 @@
+def triangle_area(base,heigth):
+    return 0.5 * base * heigth
+
+area=-1
+if area >= 10:
+    print("the space is large")
+elif area < 10 and area >0:
+    print("the space is small")
+else:
+    print("invalid")
