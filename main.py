c64efcfc87c660416c686788f7791c2afd9b303a
ismailiyad <ismailiyad@gmail,com>
2023-08-21 07:26:16 +0300

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-average = 90
-if average == 90:
-    print("Exellent")
+def check_area(area):
+    print(f"Area is {area}")
+
+    if area >= 10:
+        print("area is bigger than 10")
+    elif area >= 0:
+        print("area is maller than 10")
+    else:
+        print("invalid area")
 
 
-print("hello"
+
+
+def triangle_area(base,height):
+    area = 0.5 * base * height
+    check_area(area)
+
+def circle_area(radius):
+    area = 3.14 * (radius ** 2)
+    check_area(area)
+
+def rectangle_area(length,width):
+    area = length * width
+    check_area(area)
+
+
+triangle_area(20,2)
+circle_area(15)
+rectangle_area(5,7)
 
 
 
